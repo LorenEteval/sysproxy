fbdad1a737fd9691cfa459c6f6101bb496e958b6
Loren Eteval <loren.eteval@proton.me>
2024-02-28 20:27:49 +0800

    Update version to 0.1.2
    Signed-off-by: Loren Eteval <loren.eteval@proton.me>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 
 setup(
     name='sysproxy',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     description='Python bindings for shadowsocks sysproxy utility.',
     long_description=long_description,
